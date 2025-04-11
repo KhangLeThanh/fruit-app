1a2a0481f4954a5efe606507061cb175bdc56161
Khang Le Thanh <khanglethanh@Mac.home>
2025-04-11 14:20:52 +0300

    modify unit test for confirmation dialog

--- tests/unit/ConfirmationDialog.spec.ts
+++ tests/unit/ConfirmationDialog.spec.ts
@@ -1,16 +1,19 @@
-import { mount } from "@vue/test-utils";
+import { mount, VueWrapper } from "@vue/test-utils";
 import ConfirmationDialog from "@/components/ConfirmationDialog/ConfirmationDialog.vue"; // adjust the path as needed
 
 describe("ConfirmationDialog.vue", () => {
-  it("renders the modal when internalOpen is true", async () => {
-    const wrapper = mount(ConfirmationDialog, {
+  let wrapper: VueWrapper;
+
+  beforeEach(() => {
+    wrapper = mount(ConfirmationDialog, {
       props: {
         isVisible: true,
         message: "Are you sure you want to delete this item?",
       },
+      emit: ["confirm", "cancel"], // You can mock these events too
     });
-
-    // Check if modal is rendered
+  });
+  it("renders the modal when internalOpen is true", async () => {
     const modalContent = wrapper.find(".modal-content");
     expect(modalContent.exists()).toBe(true);
     expect(modalContent.text()).toContain(
@@ -19,58 +22,30 @@ describe("ConfirmationDialog.vue", () => {
   });
 
   it("does not render the modal when internalOpen is false", async () => {
-    const wrapper = mount(ConfirmationDialog, {
-      props: {
-        isVisible: false,
-        message: "Are you sure you want to delete this item?",
-      },
-    });
-
-    // Modal should not be visible
+    await wrapper.setProps({ isVisible: false });
     const modalContent = wrapper.find(".modal-content");
     expect(modalContent.exists()).toBe(false);
   });
 
   it('emits "confirm" event when the delete button is clicked', async () => {
-    const wrapper = mount(ConfirmationDialog, {
-      props: {
-        isVisible: true,
-        message: "Are you sure you want to delete this item?",
-      },
-    });
-
     await wrapper.find(".btn-danger").trigger("click"); // Trigger the delete button click
-
     // Expect the "confirm" event to be emitted
-    expect(wrapper.emitted("confirm")).toBeTruthy();
+    const emittedConfirm = wrapper.emitted("confirm");
+    expect(emittedConfirm).toBeTruthy();
+    expect(emittedConfirm?.length).toBe(1);
   });
 
   it('emits "cancel" event when the cancel button is clicked', async () => {
-    const wrapper = mount(ConfirmationDialog, {
-      props: {
-        isVisible: true,
-        message: "Are you sure you want to delete this item?",
-      },
-    });
-
     await wrapper.find(".btn-primary").trigger("click"); // Trigger the cancel button click
-
     // Expect the "cancel" event to be emitted
-    expect(wrapper.emitted("cancel")).toBeTruthy();
+    const emittedCancel = wrapper.emitted("cancel");
+    expect(emittedCancel).toBeTruthy();
+    expect(emittedCancel?.length).toBe(1);
   });
 
   it('emits "update:isVisible" when internalOpen changes', async () => {
-    const wrapper = mount(ConfirmationDialog, {
-      props: {
-        isVisible: true,
-        message: "Are you sure you want to delete this item?",
-      },
-      emit: ["update:isVisible"], // Mock emitting update
-    });
-
     // Update internalOpen state, simulating closing the modal
     await wrapper.setProps({ isVisible: false });
-
     // Ensure that the "update:isVisible" event is emitted
     expect(wrapper.emitted("update:isVisible")).toBeTruthy();
 
