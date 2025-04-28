39ad569924d553319bbc592f329264cb4a5d49c6
Khang Le Thanh <khanglethanh@Mac.home>
2025-04-28 15:01:20 +0300

    creating select component

---
+++ src/components/SelectComponent/SelectComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <select id="test-dropdown" :valuel="modelValue" @change="onChange">
+    <option v-for="option in options" :key="option" :value="option">
+      {{ option }}
+    </option>
+  </select>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+export default defineComponent({
+  name: "SelectComponent",
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+  },
+  emits: ["update:modelValue"],
+  setup(props, { emit }) {
+    const onChange = (event: Event) => {
+      const target = event.target as HTMLSelectElement;
+      emit("update:modelValue", target.value);
+    };
+    return { onChange };
+  },
+});
+</script>
